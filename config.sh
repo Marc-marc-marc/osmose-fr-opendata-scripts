ad4d96d13e5fa9a120b47c63c7e6e484e0fea5ac
Nicolas Cornu <nicolac76@yahoo.fr>
2021-04-29 14:15:37 +0200

    Use URL_FRONTEND_UPDATE for all requests on osmose frontend

--- config.sh
+++ config.sh
@@ -5,3 +5,5 @@ else
   echo "file $(dirname ${0})/osmose_config_password.sh not found. setting empty password"
   export OSMOSEPASS=""
 fi
+
+export URL_FRONTEND_UPDATE="http://osmose.openstreetmap.fr/control/send-update"
